8c0dfc5f5f9563c32ac5b883afd773d4f9a124b5
Marcus Chiu <marcuschiu9@gmail.com>
2020-12-24 09:55:59 -0600

    migrated to java 11 and spring cloud Hoxton.SR9

--- src/main/java/com/marcuschiu/ratingservice/RatingServiceApplication.java
+++ src/main/java/com/marcuschiu/ratingservice/RatingServiceApplication.java
@@ -5,13 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableEurekaClient
